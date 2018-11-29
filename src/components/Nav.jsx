7e600c3daddddce809cb39ae7125ad4fde2769e6
m17805804790 <1144951039@qq.com>
2018-11-29 16:26:56 +0800

    123

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ class Nav extends Component{
     render(){
         
         return(
-            <div className='navbar navbar-expand-md bg-dark navbar-dark' id='navid'>
+            <div className='navbar navbar-expand-md navbar-dark navid bg-dark '  >
                 <a className="navbar-brand" href="/">永老无别离的个人博客</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                     <span className="navbar-toggler-icon"></span>
